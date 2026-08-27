5c90789a73afe704c7c5efc18d73ba7ee4c2680f
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-08-27 20:45:56 +0200

    Add new test

---
+++ bad_entry_test.py
@@ -0,0 +1,23 @@
+from maze_generator import MazeGenerator
+
+if __name__ == "__main__":
+
+    maze = MazeGenerator(
+        15, 15, (4, 5), (3, 3), 0.75, perfect=False, perfect_centered=True
+        )
+
+    from collections import deque
+
+    # counter = 1
+    # for _ in maze.generator():
+    #     for val in _:
+    #         print(to_hex(val.walls), end="")
+    #         if counter == 9:
+    #             counter = 1
+    #             print()
+    #             continue
+    #         counter += 1
+    #     print()
+    # Exhaust the generator instantly
+    deque(maze.generator(), maxlen=0)
+    maze.transcript("maze.txt")
